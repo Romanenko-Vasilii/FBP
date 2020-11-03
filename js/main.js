1b8cb2a0dd26cd8821f50d1f67c651d3c893a961
Romanenko-Vasilii <romvas-1989@mail.ru>
2020-11-03 18:55:21 +0700

    remake slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 $('.feetback__row').slick({
   slidesToShow: 4, //сколько слайдов показывать в карусели
   slidesToScroll: 1, // сколько слайдов прокручивать за раз
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1350,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 580,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 })
